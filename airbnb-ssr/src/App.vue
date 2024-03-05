8551ab5179669e369f8c55b15c28e6cc484483be
zhaojisen <10357640+zhaojisen@user.noreply.gitee.com>
2024-03-05 17:28:34 +0800

    ✨新增indexDB 相关内容

--- airbnb-ssr/src/App.vue
+++ airbnb-ssr/src/App.vue
@@ -2,14 +2,27 @@
 import { ref } from 'vue';
 import { useI18n } from 'vue-i18n';
 
+import indexDB from './utils/indexDB';
 import en from 'element-plus/dist/locale/en.mjs';
 import zhCn from 'element-plus/dist/locale/zh-cn.mjs';
 
-const { t, locale: customLocale } = useI18n();
 const locale = ref();
 const value1 = ref('');
-
 const size = ref<'default' | 'large' | 'small'>('default');
+
+const { t, locale: customLocale } = useI18n();
+
+const airDB = new indexDB('airbnb');
+
+airDB.openStore('elephant', 'id', ['nose', 'ear']);
+
+const addDBItem = () => {
+  airDB.updateItem('elephant', { nose: '33m', ear: '很大' });
+};
+
+const deleteDBItem = () => {
+  airDB.deleteItem('elephant', 2);
+};
 
 const changeLanguange = (lang: any) => {
   locale.value = lang;
@@ -23,6 +36,8 @@ const changeLanguange = (lang: any) => {
     <div>{{ t('message.home') }}</div>
     <el-button @click="changeLanguange(zhCn)">切换为中文</el-button>
     <el-button @click="changeLanguange(en)">切换为英文</el-button>
+    <el-button @click="addDBItem">新增数据</el-button>
+    <el-button @click="deleteDBItem">删除数据</el-button>
   </el-config-provider>
 </template>
 
---
+++ airbnb-ssr/src/utils/indexDB.ts
@@ -0,0 +1,95 @@
+export default class DB {
+  // 数据库名称
+  private dbName: string = '';
+  // 数据库对象
+  private db: any;
+
+  constructor(dbName: string) {
+    this.dbName = dbName;
+  }
+
+  public openStore(storeName: string, keyPath: string, indexs?: Array<string>) {
+    /**
+     * @description 打开数据库
+     * 第一个参数为数据库名称，第二个参数为数据库版本，默认为 1
+     * open 会返回一个数据库对象，该对象中包含三个方法
+     * */
+    const request = window.indexedDB.open(this.dbName, 1);
+
+    // 数据库打开成功的回调
+    request.onsuccess = (e: any) => {
+      console.log('数据库打开成功');
+      this.db = e.target.result;
+    };
+
+    // 数据库打开失败的回调
+    request.onerror = (e) => {
+      console.log(e);
+    };
+
+    // 数据库升级成功时的回调，第一次打开数据库时，该事件会优先于 onsuccess
+    request.onupgradeneeded = (event) => {
+      /**
+       * @description 创建仓库对象
+       *              第一个参数：对象仓库名称，第二个参数：一个对象，为放置仓库中的属性
+       *              keypath 为仓库的主键
+       * */
+      const { result }: any = event.target;
+      const store = result.createObjectStore(storeName, { autoIncrement: true, keyPath });
+
+      /**
+       * @description 创建索引
+       *              第一个参数：索引名称，第二个参数：索引属性，第三个参数：配置对象
+       *              unique：是否唯一
+       * */
+
+      // 索引项为非必填，所以需要校验索引是否存在
+      if (indexs && indexs.length > 0) {
+        indexs?.map((item: string) => {
+          store.createIndex(item, item, { unique: false });
+        });
+      }
+
+      store.transaction.oncomplete = () => {
+        console.log('仓库创建成功');
+      };
+    };
+  }
+
+  // 新增数据库
+  updateItem(storeName: string, data: any) {
+    /**
+     * @description 第一个参数：数据库对象名称（数组），第二个参数：操作模式（只读还是可写）
+     * */
+    const operation = this.db.transaction([storeName], 'readwrite').objectStore(storeName);
+
+    // put 接收一个对象，也就是需要插入的数据，返回一个链接对象，同样存在两个事件 success 和 error
+    // 由于 put 相同数据会造成错误，所以额外传入一个时间戳属性
+    const request = operation.put({ ...data, updateTime: new Date().getTime() });
+
+    request.onsuccess = () => {
+      console.log('数据写入/修改成功');
+    };
+
+    request.onerror = () => {
+      console.log('数据写入/修改失败');
+    };
+  }
+
+  // 删除数据
+  deleteItem(storeName: string, key: number | string) {
+    const operation = this.db.transaction([storeName], 'readwrite').objectStore(storeName);
+
+    const request = operation.delete(key);
+
+    request.onsuccess = () => {
+      console.log('数据删除成功');
+    };
+
+    request.onerror = () => {
+      console.log('数据删除失败');
+    };
+  }
+
+  // 查询数据，查询整个对象仓库的数据和查询某一条的数据
+}
